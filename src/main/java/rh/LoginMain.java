8c1ba99eed92055c6274e3909ec693f35f2c3da5
haendel721 <bramwalker2@gmail.com>
2025-08-22 17:29:44 +0300

    Créeation de l'interface login

--- src/main/java/rh/LoginMain.java
+++ src/main/java/rh/LoginMain.java
@@ -4,20 +4,30 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Dialog;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
 
 public class    LoginMain extends Application {
-    //OK
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        // Charge le FXML depuis le bon chemin
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Login/Login.fxml"));
-        Scene scene = new Scene(root);
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("Login System");
-        primaryStage.show();
-    }
 
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Login/loginPro.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+
+        // Enlever les boutons natifs
+        stage.initStyle(StageStyle.UNDECORATED);
+
+        // Charger le CSS
+        scene.getStylesheets().add(getClass().getResource("/css/login.css").toExternalForm());
+
+        stage.setScene(scene);
+        stage.setTitle("Formulaire de connexion");
+        stage.show();
+    }
     public static void main(String[] args) {
         launch(args);
     }
